51e46f73129b0b4fc96baa47962c7dfafff11029
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2020-08-11 17:00:47 -0700

    Added insert and contains methods to BST

--- bst_construction.rb
+++ bst_construction.rb
@@ -7,12 +7,45 @@ class BST
     @right = nil
   end
 
+# Average: O(log(n)) t | O(1) s
+# Worst: O(n) t | O(1) s
   def insert(value)
-
+    current_node = self
+    while true
+      if value < current_node.value
+        if current_node.left.nil?
+          current_node.left = BST.new(value)
+          break
+        else
+          current_node = current_node.left
+        end
+      else
+        if current_node.right.nil?
+          current_node.right = BST.new(value)
+          break
+        else
+          current_node = current_node.right
+        end
+      end
+    end
+    return self
   end
 
 
   def contains(value)
+    current_node = self
+
+    while !current_node.nil?
+      if value < current_node.value
+        current_node = current_node.left
+      elsif value > current_node.value
+        current_node = current_node.right
+      else
+        return true
+      end
+    end
+
+    return false
 
   end
 
@@ -35,11 +68,11 @@ root.right.left.right = BST.new(14)
 root.right.right = BST.new(22)
 
 
-root.insert(12)
-puts "Should output true: #{root.right.left.left.value == 12}"
+# root.insert(12)
+# puts "Should output true: #{root.right.left.left.value == 12}"
 
-root.remove(10)
-puts "Should output false: #{root.contains(10)}"
+# root.remove(10)
+# puts "Should output false: #{root.contains(10)}"
 
 puts "Should output true: #{root.contains(15)}"
 
