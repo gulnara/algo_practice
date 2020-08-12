5fe31f8c300279a7f7699fbfdba75848db5db3d3
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2020-08-12 08:03:58 -0700

    complete bst_traversal

--- bst_traversal.rb
+++ bst_traversal.rb
@@ -10,16 +10,37 @@ end
 
 #  O(n) time | O(n) space 
 def in_order_traverse(tree, array)
+  if !tree.nil?
+    in_order_traverse(tree.left, array)
+    array.append(tree.value)
+    in_order_traverse(tree.right, array)
+  end
+  return array
+
 
 end
 
 #  O(n) time | O(n) space 
 def pre_order_traverse(tree, array)
+  if !tree.nil?
+    array.append(tree.value)
+    pre_order_traverse(tree.left, array)
+    pre_order_traverse(tree.right, array)
+  end
+  
+  return array
 
 end
 
 #  O(n) time | O(n) space 
 def post_order_traverse(tree, array)
+  if !tree.nil?
+    post_order_traverse(tree.left, array)
+    post_order_traverse(tree.right, array)
+    array.append(tree.value)
+  end
+  
+  return array
 
 end
 
@@ -30,14 +51,12 @@ root.left.left = BST.new(2)
 root.left.left.left = BST.new(1)
 root.left.right = BST.new(5)
 root.right = BST.new(15)
-root.right.left = BST.new(13)
-root.right.left.right = BST.new(14)
 root.right.right = BST.new(22)
 
 inOrder = [1, 2, 5, 5, 10, 15, 22]
 preOrder = [10, 5, 2, 1, 5, 15, 22]
 postOrder = [1, 2, 5, 5, 22, 15, 10]
 
-puts "Should output true: #{in_order_traverse(tree, []) == inOrder}"
-puts "Should output true: #{pre_order_traverse(tree, []) == preOrder}"
-puts "Should output true: #{post_order_traverse(tree, []) == postOrder}"
+puts "Should output true: #{in_order_traverse(root, []) == inOrder}"
+puts "Should output true: #{pre_order_traverse(root, []) == preOrder}"
+puts "Should output true: #{post_order_traverse(root, []) == postOrder}"
