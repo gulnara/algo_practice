d81e5edd56a8d74e0fb191b9044a8037d09fa639
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2020-08-18 11:21:33 -0700

    Added search, remove and remove with value methods to linked lists

--- linked_lists/linked_list_construction.rb
+++ linked_lists/linked_list_construction.rb
@@ -18,35 +18,76 @@ class DoublyLinkedList
     @tail = nil
   end
 
-  def setHead(node)
+  def set_head(node)
     
   end
 
-  def setTail(node)
+  def set_tail(node)
     
   end
 
-  def insertBefore(node, nodeToInsert)
+  def insert_before(node, node_to_insert)
     
   end
 
-  def insertAfter(node, nodeToInsert)
+  def insert_after(node, node_to_insert)
     
   end
 
-  def insertAtPosition(position, nodeToInsert)
+  def insert_at_position(position, node_to_insert)
     
   end
 
-  def removeNodesWithValue(value)
-    
+# O(n) time | O(1) space
+  def remove_nodes_with_value(value)
+    node = self.head
+    while !node.nil?
+      node_to_remove = node
+      node = node.next
+      if node_to_remove.value == value
+        self.remove(node_to_remove)
+      end
+    end
   end
 
+# O(1) time | O(1) space
   def remove(node)
-    
+    if node == self.head
+      self.head = self.head.next
+    end
+
+    if node == self.tail
+      self.tail = self.tail.prev
+    end
+    self.remove_bindings(node)
+
   end
 
-  def containsNodeWithValue(value)
-    
+# O(n) time | O(1) space
+  def contains_node_with_value(value)
+    node = self.head
+    while !node.nil? && node.value != value
+      node = node.next
+    end
+    return !node.nil?
   end
+
+  def remove_bindings(node)
+    if !node.prev.nil?
+      node.prev.next = node.next
+    end
+
+    if !node.next.nil?
+      node.next.prev = node.prev
+    end
+    node.prev = nil
+    nodex.next = nil
+
+  end
+
 end
+
+
+
+
+
