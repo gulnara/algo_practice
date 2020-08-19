ac511ac5201bd7068a07d3719de45a009648c56c
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2020-08-19 10:33:11 -0700

    initial min heap construct

---
+++ heaps/min_heap_construction.rb
@@ -0,0 +1,29 @@
+class MinHeap
+  def initialize(array)
+    self.heap = self.buildHeap(array)
+  end
+
+  def buildHeap(array)
+    
+  end
+
+  def siftDown
+    
+  end
+
+  def siftUp
+    
+  end
+
+  def peek
+    
+  end
+
+  def remove
+    
+  end
+
+  def insert(value)
+    
+  end
+end
