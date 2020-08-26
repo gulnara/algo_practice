473f3b0174b3ad52beecff3f0f483a1ee47b8dc1
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2020-08-26 09:40:41 -0700

    initial heap sort logic

---
+++ sorting/heap_sort.rb
@@ -0,0 +1,12 @@
+# O(nlog(n)) t | O(1) s
+
+def heap_sort(array)
+
+end
+
+
+
+array = [8, 5, 2, 9, 5, 6, 3]
+expected = [2, 3, 5, 5, 6, 8, 9]
+
+puts "Should output true: #{heap_sort(array) == expected}"
