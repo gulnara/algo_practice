20f48ad8469935554207ec3a33e1cb0b89eec8e1
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2020-08-28 09:02:08 -0700

    initial search in range

---
+++ searching/search_for_range.rb
@@ -0,0 +1,12 @@
+
+
+def search_for_range(array, target)
+
+end
+
+
+
+array = [0, 1, 21, 33, 45, 45, 45, 45, 45, 45, 61, 71, 73]
+puts "Should output true: #{search_for_range(array, 45) == [4,9]}"
+
+
--- searching/shifted_binary_search.rb
+++ searching/shifted_binary_search.rb
@@ -72,3 +72,5 @@ end
 
 array = [45, 61, 71, 72, 73, 0, 1, 21, 33, 37]
 puts "Should output true: #{shifted_binary_search(array, 33) == 8}"
+
+
