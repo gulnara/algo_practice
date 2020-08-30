e714c9556e1c97cf2b745ad4fa186ffb0673754d
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2020-08-30 09:08:58 -0700

    merge linked lists initial setup

---
+++ linked_lists/merge_linked_list.rb
@@ -0,0 +1,16 @@
+
+class LinkedList
+
+  attr_accessor :value, :next
+
+  def initialize(value)
+    @value = value
+    @next = nil
+  end
+end
+
+# itirative
+def merge_linked_list(head_one, head_two)
+
+
+end
