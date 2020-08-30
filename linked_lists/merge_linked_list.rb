c802670a7cd87d34feb7251acfff5d4f12c6cd34
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2020-08-30 09:25:38 -0700

    itirative solution

--- linked_lists/merge_linked_list.rb
+++ linked_lists/merge_linked_list.rb
@@ -10,7 +10,37 @@ class LinkedList
 end
 
 # itirative
+#  O(n+m) time | O(1) space
 def merge_linked_list(head_one, head_two)
+  p1 = head_one
+  p1_prev = nil
+  p2 = head_two
 
+  while !p1.nil? and !p2.nil?
+    if p1.value < p2.value
+      p1_prev = p1
+      p1 = p1.next
+    else
+      if !p1_prev.nil?
+        p1_prev.next = p2
+      end
+      p1_prev = p2
+      p2 = p2.next
+      p1_prev.next = p1
+    end
+  end
+
+  if p1.nil?
+    p1_prev.next = p2
+  end
+
+  if head_one.value < head_two.value
+    return head_one
+  else
+    return head_two
+  end
 
 end
+
+
+
