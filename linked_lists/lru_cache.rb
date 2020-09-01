0d8d5085eab3e84a5a9ecad9f773d96bf638c502
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2020-08-31 19:53:43 -0700

    lru cache initial setup

---
+++ linked_lists/lru_cache.rb
@@ -0,0 +1,22 @@
+
+class LRUCache
+
+  attr_accessor :max_size
+
+  def initialize(max_size)
+    @max_size = max_size || 1
+  end
+
+  def insert_key_value_pair(key, value)
+
+  end
+
+  def get_value_from_key(key)
+
+  end
+
+  def get_most_recent_key
+
+  end
+
+end
