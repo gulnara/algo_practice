4b77be0f407b2de5e337ca07820abf3c0b65dab9
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2020-09-03 08:16:59 -0700

    uncommented out solution

--- strings/palindrome.rb
+++ strings/palindrome.rb
@@ -1,35 +1,35 @@
 
 #  O(n^2) T | O(n) S
-# def is_palindrome(string)
-#   reversed_string = ""
-#   for i in (string.length-1).downto(0) do
-#     puts i
-#     reversed_string += string[i]
-#   end
-#   return string == reversed_string
+def is_palindrome(string)
+  reversed_string = ""
+  for i in (string.length-1).downto(0) do
+    puts i
+    reversed_string += string[i]
+  end
+  return string == reversed_string
 
-# end
-
-#  O(n) T | O(n) S
-# def is_palindrome(string)
-#   reversed_chars = []
-#   for i in (string.length-1).downto(0) do
-#     reversed_chars.append(string[i])
-#   end
-#   return string == reversed_chars.join
-
-# end
-
+end
 
 #  O(n) T | O(n) S
-# def is_palindrome(string, i = 0)
-#   j = string.length - 1 - i
-#   if i >= j 
-#     return true
-#   else 
-#     string[i] == string[j] && is_palindrome(string, i + 1)
-#   end
-# end
+def is_palindrome(string)
+  reversed_chars = []
+  for i in (string.length-1).downto(0) do
+    reversed_chars.append(string[i])
+  end
+  return string == reversed_chars.join
+
+end
+
+
+#  O(n) T | O(n) S
+def is_palindrome(string, i = 0)
+  j = string.length - 1 - i
+  if i >= j 
+    return true
+  else 
+    string[i] == string[j] && is_palindrome(string, i + 1)
+  end
+end
 
 
 #  O(n) T | O(1) S
