6ea8908cbd48a224c3b11f81fa56ea089df00cc1
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2020-09-04 13:46:56 -0700

    most used word

---
+++ most_used_word.rb
@@ -0,0 +1,12 @@
+
+
+
+def find_most_used(string, words_to_exclude)
+
+end
+
+
+string = "Find a word with that word find s word's most the and common That!"
+words_to_exclude = ["and", "A", "With", "the"]
+
+output = ["word"]
