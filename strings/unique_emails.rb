9e1e7ed71b1fc964865cbd8df8c279907a9eebcd
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2020-10-16 08:39:06 -0700

    unique emails

---
+++ strings/unique_emails.rb
@@ -0,0 +1,22 @@
+# @param {String[]} emails
+# @return {Integer}
+def num_unique_emails(emails)
+    seen = {}
+    counter = 0
+
+    emails.each do |email|
+      local, domain = email.split('@')
+      local = local.gsub('.', '').split('+')[0]
+      seen[domain] ||= []
+      if !seen[domain].include?(local)
+        seen[domain] << local
+        counter += 1
+      end
+    end
+
+    return counter
+    
+end
+
+emails = ["test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"]
+num_unique_emails(emails)
