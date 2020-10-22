cf5c6298425078f19603f7071da674f335b283a1
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2020-10-22 08:20:41 -0700

    license key formating

--- strings/license_key_formatting.rb
+++ strings/license_key_formatting.rb
@@ -1,33 +1,48 @@
 # @param {String} s
 # @param {Integer} k
 # @return {String}
-def license_key_formatting(s, k)
-  new_string = ""
-  stripped_string = s.gsub("-","")
+# def license_key_formatting(s, k)
+#   new_string = ""
+#   stripped_string = s.gsub("-","")
 
-  if stripped_string.length % k == 0
-    rest_sub_string = stripped_string
-  else
-    new_string = stripped_string[0..(stripped_string.length % k - 1)]
-    rest_sub_string = stripped_string[(stripped_string.length % k)..]
-  end
+#   if stripped_string.length % k == 0
+#     rest_sub_string = stripped_string
+#   else
+#     new_string = stripped_string[0..(stripped_string.length % k - 1)]
+#     rest_sub_string = stripped_string[(stripped_string.length % k)..]
+#   end
 
-  begin_index = 0
-  end_index = k - 1
+#   begin_index = 0
+#   end_index = k - 1
 
-  while begin_index < rest_sub_string.length do
-    if new_string == ""
-      new_string = new_string + rest_sub_string[begin_index..end_index].upcase
-    else
-      new_string = new_string.upcase + "-" + rest_sub_string[begin_index..end_index].upcase
-    end 
+#   while begin_index < rest_sub_string.length do
+#     if new_string == ""
+#       new_string = new_string + rest_sub_string[begin_index..end_index].upcase
+#     else
+#       new_string = new_string.upcase + "-" + rest_sub_string[begin_index..end_index].upcase
+#     end 
 
-    begin_index = end_index + 1
-    end_index = begin_index + k - 1
-  end
+#     begin_index = end_index + 1
+#     end_index = begin_index + k - 1
+#   end
  
+#   return new_string
+# end
 
-  return new_string
+def license_key_formatting(s, k)
+  s.upcase.tr('-', '').reverse.scan(/.{1,#{k}}/).join('-').reverse
+end
+
+def license_key_formatting(s, k)
+    s = s.split("-").join("").upcase()
+    str = []
+    num = s.size() % k
+    str << s[0..num-1] if num > 0
+    while num < s.size()
+        str << s[num..num+k-1]
+        num += k
+    end
+    str.join("-")
 end
 
 s = "a-a-a-a-"
