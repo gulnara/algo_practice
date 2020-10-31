7775368b721a21ee963b042f5ea728ae65a0c086
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2020-10-31 16:53:32 -0700

    setup for longest substring of distinct chars

---
+++ sliding_window/longest_substring_distinct_chars.rb
@@ -0,0 +1,9 @@
+def longest_substring_with_k_distinct(str1, k)
+
+end
+
+puts "Length of the longest substr1ing: " + longest_substring_with_k_distinct("araaci", 2).to_s
+
+puts "Length of the longest substr1ing: " + longest_substring_with_k_distinct("araaci", 1).to_s
+
+puts "Length of the longest substr1ing: " + longest_substring_with_k_distinct("cbbebi", 3).to_s
