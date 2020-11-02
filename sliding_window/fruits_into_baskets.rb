2e69a9b324caf6fbd58a583c1f5df60b3fa44672
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2020-11-02 09:10:30 -0800

    added solution

--- sliding_window/fruits_into_baskets.rb
+++ sliding_window/fruits_into_baskets.rb
@@ -7,6 +7,28 @@
 
 def fruits_into_baskets(fruits)
 
+  fruit_baskets = {}
+  start_index = 0
+  max_length = 0
+
+  for end_index in 0..(fruits.length - 1)
+    fruit = fruits[end_index]
+    if !fruit_baskets.include?(fruit)
+      fruit_baskets[fruit] = 0
+    end
+    fruit_baskets[fruit] =+ 1
+
+    while fruit_baskets.length > 2
+      left_fruit = fruits[start_index]
+      fruit_baskets[left_fruit] -= 1
+      if fruit_baskets[left_fruit] == 0
+        fruit_baskets.delete(left_fruit)
+      end
+      start_index += 1
+    end
+    max_length = [max_length, end_index - start_index + 1].max
+  end
+  return max_length
 end
 
 puts "Maximum number of fruits: " + fruits_into_baskets(['A', 'B', 'C', 'A', 'C']).to_s
