84df911d5596addae767c291b67ff200057113ce
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2020-11-02 19:03:21 -0800

    longest substring problem setup

---
+++ sliding_window/non_repeat_substring.rb
@@ -0,0 +1,9 @@
+# Given a string, find the length of the longest substring which has no repeating characters.
+def non_repeat_substring(str)
+  
+end
+
+
+"Length of the longest substring: " + non_repeat_substring("aabccbb").to_s
+"Length of the longest substring: " + non_repeat_substring("abbbb").to_s
+"Length of the longest substring: " + non_repeat_substring("abccde").to_s
