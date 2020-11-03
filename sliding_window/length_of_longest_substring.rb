83aeeb555b57f806a411216973225b06482758ab
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2020-11-02 20:32:51 -0800

    problem setup

--- sliding_window/length_of_longest_substring.rb
+++ sliding_window/length_of_longest_substring.rb
@@ -1,3 +1,5 @@
+# Given a string with lowercase letters only, if you are allowed to replace no more than ‘k’ letters with any letter, find the length of the longest substring having the same letters after replacement.
+
 def length_of_longest_substring(str, k)
 
 end
