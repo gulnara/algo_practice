7bdd0ce257df4c8c44b4bf111c274f3d07b9b43b
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2020-11-02 19:15:05 -0800

    added solution

--- sliding_window/non_repeat_substring.rb
+++ sliding_window/non_repeat_substring.rb
@@ -1,9 +1,23 @@
 # Given a string, find the length of the longest substring which has no repeating characters.
 def non_repeat_substring(str)
-  
+  distinct_chars = {}
+  start_index = 0
+  max_length = 0
+
+  for end_index in 0..(str.length - 1)
+    right_char = str[end_index]
+
+    if distinct_chars.include?(right_char)
+      start_index = [start_index, distinct_chars[right_char] + 1].max
+    end
+    distinct_chars[right_char] = end_index
+    max_length = [max_length, end_index - start_index + 1].max
+  end
+
+  return max_length
 end
 
 
-"Length of the longest substring: " + non_repeat_substring("aabccbb").to_s
-"Length of the longest substring: " + non_repeat_substring("abbbb").to_s
-"Length of the longest substring: " + non_repeat_substring("abccde").to_s
+puts "Length of the longest substring: aabccbb = 3 #{(non_repeat_substring("aabccbb") == 3)}"
+puts "Length of the longest substring: abbbb = 2 #{(non_repeat_substring("abbbb") == 2)}"
+puts "Length of the longest substring: abccde = 3 #{(non_repeat_substring("abccde") == 3)}"
