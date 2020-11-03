a297f5c467eff94b5477396518ba56e2c2fb0f92
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2020-11-03 10:36:13 -0800

    solution

--- sliding_window/length_of_longest_substring_0_1.rb
+++ sliding_window/length_of_longest_substring_0_1.rb
@@ -2,4 +2,26 @@
 
 def length_of_longest_substring(arr, k)
 
+  start_index, max_length, max_ones = 0, 0, 0
+
+  for end_index in 0..(arr.length - 1)
+    if arr[end_index] == 1
+      max_ones += 1
+    end
+
+    if (end_index - start_index + 1 - max_ones) > k
+      if arr[start_index] == 1
+        max_ones -= 1
+      end
+      start_index += 1
+    end
+
+    max_length = [max_length, end_index - start_index + 1].max
+  end
+
+  return max_length
 end
+
+
+puts length_of_longest_substring([0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1], 2) == 6
+puts length_of_longest_substring([0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1], 3) == 9
