e91d35802b8e3eacaa88b809e7128e4b79920e02
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2020-11-02 19:16:30 -0800

    added complexities

--- sliding_window/non_repeat_substring.rb
+++ sliding_window/non_repeat_substring.rb
@@ -1,4 +1,8 @@
 # Given a string, find the length of the longest substring which has no repeating characters.
+
+# The space complexity of the algorithm will be O(K)O(K) where KK is the number of distinct characters in the input string. This also means K<=NK<=N, because in the worst case, the whole string might not have any repeating character so the entire string will be added to the HashMap. Having said that, since we can expect a fixed set of characters in the input string (e.g., 26 for English letters), we can say that the algorithm runs in fixed space O(1)O(1); in this case, we can use a fixed-size array instead of the HashMap.
+
+# O(n) T | O(1) S
 def non_repeat_substring(str)
   distinct_chars = {}
   start_index = 0
