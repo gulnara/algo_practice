1b68ee445b2d9495078d038ae131ec217ea60c44
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2020-11-03 08:30:25 -0800

    added complexity

--- sliding_window/length_of_longest_substring.rb
+++ sliding_window/length_of_longest_substring.rb
@@ -1,5 +1,7 @@
 # Given a string with lowercase letters only, if you are allowed to replace no more than ‘k’ letters with any letter, find the length of the longest substring having the same letters after replacement.
 
+# O(n) T | O(1) S
+
 def length_of_longest_substring(str, k)
   start_index, max_length, max_repeat_letter_count = 0, 0 , 0
   char_freq = {}
