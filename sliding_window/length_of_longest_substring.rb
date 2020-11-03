8cd58538d22f9abbfe7e0883312e1e0c91e1efbd
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2020-11-02 20:32:30 -0800

    length of longest substring

---
+++ sliding_window/length_of_longest_substring.rb
@@ -0,0 +1,7 @@
+def length_of_longest_substring(str, k)
+
+end
+
+puts length_of_longest_substring("aabccbb", 2) == 5
+puts length_of_longest_substring("abbcb", 1) == 4
+puts length_of_longest_substring("abccde", 1) == 3
