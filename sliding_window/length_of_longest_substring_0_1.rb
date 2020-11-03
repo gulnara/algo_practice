79f29142b74658f0c1dbda2d134fc1a4d4c79296
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2020-11-03 08:39:45 -0800

    Given an array containing 0s and 1s, if you are allowed to replace no more than ‘k’ 0s with 1s, find the length of the longest contiguous subarray having all 1s.

---
+++ sliding_window/length_of_longest_substring_0_1.rb
@@ -0,0 +1,5 @@
+# Given an array containing 0s and 1s, if you are allowed to replace no more than ‘k’ 0s with 1s, find the length of the longest contiguous subarray having all 1s.
+
+def length_of_longest_substring(arr, k)
+
+end
