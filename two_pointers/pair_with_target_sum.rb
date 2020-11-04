290b4eb885045469610d58927eedc718ea81669a
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2020-11-04 11:29:05 -0800

    added problem setup for pair with targetsum

---
+++ two_pointers/pair_with_target_sum.rb
@@ -0,0 +1,7 @@
+# Given an array of sorted numbers and a target sum, find a pair in the array whose sum is equal to the given target.
+
+# Write a function to return the indices of the two numbers (i.e. the pair) such that they add up to the given target.
+
+def pair_with_targetsum(arr, target_sum)
+
+end
