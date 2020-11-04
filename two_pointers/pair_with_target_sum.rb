94107a05fd2814549c5257a22df069a050e61e73
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2020-11-04 11:36:41 -0800

    added solution

--- two_pointers/pair_with_target_sum.rb
+++ two_pointers/pair_with_target_sum.rb
@@ -3,5 +3,21 @@
 # Write a function to return the indices of the two numbers (i.e. the pair) such that they add up to the given target.
 
 def pair_with_targetsum(arr, target_sum)
+  left = 0
+  right = arr.length - 1
 
+  while left < right do
+    if arr[left] + arr[right] < target_sum
+      left += 1
+    elsif arr[left] + arr[right] > target_sum
+      right -= 1
+    elsif arr[left] + arr[right] == target_sum
+      return [left, right]
+    else
+      return 'There is no pair of int that add up to target sum'
+    end
+  end
 end
+
+puts pair_with_targetsum([1, 2, 3, 4, 6], 6) == [1, 3]
+puts pair_with_targetsum([2, 5, 9, 11], 11) == [0, 2]
