f22a818904a7d3e1f0ccd5bd50acfb68e1e78b91
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2020-11-04 16:07:59 -0800

    added solution

--- two_pointers/remove_duplicates.rb
+++ two_pointers/remove_duplicates.rb
@@ -1,8 +1,18 @@
 # Given an array of sorted numbers, remove all duplicates from it. You should not use any extra space; after removing the duplicates in-place return the length of the subarray that has no duplicate in it.
 
 def remove_duplicates(arr)
+  next_non_duplicate = 1
+  i = 1
 
+  while i < arr.length
+    if arr[next_non_duplicate - 1] != arr[i]
+      arr[next_non_duplicate] = arr[i]
+      next_non_duplicate += 1
+    end
+    i += 1
+  end
+  return next_non_duplicate
 end
 
-puts remove_element([3, 2, 3, 6, 3, 10, 9, 3], 3) == 4
-puts remove_element([2, 11, 2, 2, 1], 2) == 2
+puts remove_duplicates([2, 3, 3, 3, 6, 9, 9]) == 4
+puts remove_duplicates([2, 2, 2, 11]) == 2
