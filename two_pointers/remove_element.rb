bfe2720f03d76e6700a696d6ce52c17d2962a7de
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2020-11-04 16:55:57 -0800

    remove_element problem setup'

---
+++ two_pointers/remove_element.rb
@@ -0,0 +1,8 @@
+# This problem is quite similar to our parent problem. We can follow a two-pointer approach and shift numbers left upon encountering the ‘key’. Here is what the code will look like
+
+def remove_element(arr, key)
+
+end
+
+puts remove_element([3, 2, 3, 6, 3, 10, 9, 3], 3) == 4
+puts remove_element([2, 11, 2, 2, 1], 2) == 2
