e9ee571ff57e3aec778cd5934945874649ccf643
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2020-11-04 17:04:04 -0800

    added solution

--- two_pointers/remove_element.rb
+++ two_pointers/remove_element.rb
@@ -1,7 +1,16 @@
 # This problem is quite similar to our parent problem. We can follow a two-pointer approach and shift numbers left upon encountering the ‘key’. Here is what the code will look like
 
 def remove_element(arr, key)
+  next_element = 0
 
+  for i in 0..(arr.length - 1) 
+    if arr[i] != key
+      arr[next_element] = arr[i]
+      next_element += 1
+    end
+
+  end
+  return next_element
 end
 
 puts remove_element([3, 2, 3, 6, 3, 10, 9, 3], 3) == 4
