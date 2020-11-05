8a82538467ae103354b62f856ffd6332ebd3fd7a
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2020-11-05 12:41:21 -0800

    make squares problem setup

---
+++ two_pointers/make_squares.rb
@@ -0,0 +1,9 @@
+# Given a sorted array, create a new array containing squares of all the number of the input array in the sorted order.
+
+
+def make_squares(arr)
+
+end
+
+puts make_squares([-2, -1, 0, 2, 3]) == [0, 1, 4, 4, 9]
+puts make_squares([-3, -1, 0, 1, 2]) == [0, 1, 1, 4, 9]
