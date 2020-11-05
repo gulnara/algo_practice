16739ed48e60847d12599180e181de06dc1e1084
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2020-11-04 16:08:29 -0800

    added complexity

--- two_pointers/remove_duplicates.rb
+++ two_pointers/remove_duplicates.rb
@@ -1,5 +1,6 @@
 # Given an array of sorted numbers, remove all duplicates from it. You should not use any extra space; after removing the duplicates in-place return the length of the subarray that has no duplicate in it.
 
+# O(n) T | O(1) S
 def remove_duplicates(arr)
   next_non_duplicate = 1
   i = 1
