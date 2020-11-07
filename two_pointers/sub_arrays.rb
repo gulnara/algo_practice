eda72783d94c6b4d919aff18b2886cb9c24d1a72
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2020-11-06 16:20:16 -0800

    subarray

---
+++ two_pointers/sub_arrays.rb
@@ -0,0 +1,9 @@
+# Given an array with positive numbers and a target number, find all of its contiguous subarrays whose product is less than the target number
+
+def find_subarrays(arr, target)
+
+end
+
+
+puts find_subarrays([2, 5, 3, 10], 30) == [[2], [5], [2, 5], [3], [5, 3], [10]]
+puts find_subarrays([8, 2, 6, 5], 50) == [[8], [2], [8, 2], [6], [2, 6], [5], [6, 5]]
