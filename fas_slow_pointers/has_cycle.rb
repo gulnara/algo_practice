f4bbd37c92c8860b4c21bddcc8bcb069629ff3ee
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2020-11-09 08:22:31 -0800

    added testing for cycles

--- fas_slow_pointers/has_cycle.rb
+++ fas_slow_pointers/has_cycle.rb
@@ -14,7 +14,7 @@ def has_cycle(head)
 
   while !fast.nil? and !fast.next.nil?
     fast = fast.next.next
-    slow = slown.next
+    slow = slow.next
     if slow == fast
       return true
     end
@@ -22,3 +22,13 @@ def has_cycle(head)
   return false
 end
 
+head = Node.new(1)
+head.next = Node.new(2)
+head.next.next = Node.new(3)
+head.next.next.next = Node.new(4)
+head.next.next.next.next = Node.new(5)
+
+puts "Does this list have a cycle?: #{has_cycle(head).to_s}"
+
+head.next.next.next.next.next = head.next.next
+puts "Does this list have a cycle?: #{has_cycle(head).to_s}"
