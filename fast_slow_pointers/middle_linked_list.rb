1bb9696aea6ec48e74eeb77d80dcd97c1936a102
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2020-11-12 08:09:00 -0800

    problem setup

--- fast_slow_pointers/middle_linked_list.rb
+++ fast_slow_pointers/middle_linked_list.rb
@@ -1,4 +1,17 @@
 
+# Given the head of a Singly LinkedList, write a method to return the middle node of the LinkedList.
+
+
+class Node
+
+  attr_accessor :value, :next
+
+  def initialize(value)
+    @value = value
+    @next = nil
+  end
+end
+
 
 def find_middle_of_linked_list(head)
 
