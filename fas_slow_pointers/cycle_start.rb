f8ee3facb64599d4edb104c51bb0d4779b0bee83
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2020-11-11 19:52:38 -0800

    added solution

--- fas_slow_pointers/cycle_start.rb
+++ fas_slow_pointers/cycle_start.rb
@@ -10,21 +10,36 @@ class Node
 end
 
 def find_cycle_start(head)
+  slow,fast = head, head
+  length = 0
 
+    while !fast.nil? && !fast.next.nil?
+      slow = slow.next
+      fast = fast.next.next
+      if fast == slow
+        length = find_length(slow)
+        break
+      end
+    end
+
+    return find_start(head, length)
 end
 
-def find_cycle(head)
-  slow,fast = head, head
+def find_start(head, length)
+  pointer1, pointer2 = head, head
 
-  while !fast.nil? && !fast.next.nil?
-    slow = slow.next
-    fast = fast.next.next
-    if fast == slow
-      return find_length(slow)
-    end
+  while length > 0
+    pointer2 = pointer2.next
+    length -= 1
   end
 
-  return 0
+  while pointer1 != pointer2
+    pointer1 = pointer1.next
+    pointer2 = pointer2.next
+  end
+
+  return pointer1
+
 end
 
 def find_length(slow)
@@ -43,7 +58,13 @@ def find_length(slow)
   return length
 end
 
-
+head = Node.new(1)
+head.next = Node.new(2)
+head.next.next = Node.new(3)
+head.next.next.next = Node.new(4)
+head.next.next.next.next = Node.new(5)
+head.next.next.next.next.next = head.next
+puts "What is a start of cycle?: #{find_cycle_start(head).value.to_s}"
 
 
 
