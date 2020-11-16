ae61dd849e3402acaefa0615adcbb687d88b0168
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2020-11-15 17:34:22 -0800

    added check for if only 1 interval

--- patterns/merge_intervals/merge_intervals.rb
+++ patterns/merge_intervals/merge_intervals.rb
@@ -1,5 +1,8 @@
 # Given a list of intervals, merge all the overlapping intervals to produce a list that has only mutually exclusive intervals.
 
 def merge(intervals)
+  if intervals.length < 2
+    return intervals
+  end
 
 end
