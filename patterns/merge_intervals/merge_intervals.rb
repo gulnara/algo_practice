71ab8e27387757fdef0e3ec4f2f4e5d31f708ebc
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2020-11-16 20:57:48 -0800

    added interval class

--- patterns/merge_intervals/merge_intervals.rb
+++ patterns/merge_intervals/merge_intervals.rb
@@ -1,5 +1,18 @@
 # Given a list of intervals, merge all the overlapping intervals to produce a list that has only mutually exclusive intervals.
 
+class Interval
+  attr_accessor :start_point, :end_point
+
+  def initialize(start_point, end_point)
+    @start_point = start_point
+    @end_point = end_point
+  end
+
+  def print_interval
+    puts "[ #{self.start_point.to_s}, #{self.end_point.to_s} ]"
+  end
+end
+
 def merge(intervals)
   if intervals.length < 2
     return intervals
