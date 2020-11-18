b919bbdc72d8e51004fae4bba2377b1425e8ec0c
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2020-11-18 08:56:21 -0800

    added more test cases

--- patterns/merge_intervals/insert_interval.rb
+++ patterns/merge_intervals/insert_interval.rb
@@ -1,2 +1,9 @@
 # Given a list of non-overlapping intervals sorted by their start time, insert a given interval at the correct position and merge all necessary intervals to produce a list that has only mutually exclusive intervals.
 
+def insert(intervals, new_interval)
+
+end
+
+puts "Intervals after inserting the new interval: #{insert([[1, 3], [5, 7], [8, 12]], [4, 6]).to_s}"
+puts "Intervals after inserting the new interval: #{insert([[1, 3], [5, 7], [8, 12]], [4, 10]).to_s}"
+puts "Intervals after inserting the new interval: #{insert([[2, 3], [5, 7]], [1, 4]).to_s}"
