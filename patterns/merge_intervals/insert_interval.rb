7588a59c0ea574fac044445185fc3ba88c234836
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2020-11-30 19:06:37 -0500

    start and end points

--- patterns/merge_intervals/insert_interval.rb
+++ patterns/merge_intervals/insert_interval.rb
@@ -1,6 +1,8 @@
 # Given a list of non-overlapping intervals sorted by their start time, insert a given interval at the correct position and merge all necessary intervals to produce a list that has only mutually exclusive intervals.
 
 def insert(intervals, new_interval)
+	merged = []
+	i, start_point, end_point = 0, 0, 1
 
 end
 
