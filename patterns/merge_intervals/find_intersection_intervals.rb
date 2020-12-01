19beeada0442adb60a3642978beb6ca49b1957e3
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2020-12-01 15:57:08 -0500

    intersection intervals merge

---
+++ patterns/merge_intervals/find_intersection_intervals.rb
@@ -0,0 +1,9 @@
+# Given two lists of intervals, find the intersection of these two lists. Each list consists of disjoint intervals sorted on their start time.
+
+def merge(intervals_a, intervals_b)
+
+end
+
+
+puts "Intervals after inserting the new interval: #{merge([[1, 3], [5, 6], [7, 9]], [[2, 3], [5, 7]]).to_s}"
+puts "Intervals after inserting the new interval: #{merge([[1, 3], [5, 7], [9, 12]], [[5, 10]]).to_s}"
