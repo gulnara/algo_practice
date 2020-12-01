94279e352bcb8dd3710cb1249a9ce1edf3c4c94a
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2020-12-01 15:54:31 -0500

    added complexity

--- patterns/merge_intervals/insert_interval.rb
+++ patterns/merge_intervals/insert_interval.rb
@@ -1,4 +1,5 @@
 # Given a list of non-overlapping intervals sorted by their start time, insert a given interval at the correct position and merge all necessary intervals to produce a list that has only mutually exclusive intervals.
+# O(N) S T
 
 def insert(intervals, new_interval)
 	merged = []
