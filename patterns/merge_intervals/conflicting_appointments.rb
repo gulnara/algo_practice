b3a1bdb3eef0f9be3ca36a3884dae5972d6f8f2e
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2020-12-02 10:01:21 -0500

    new problem setup

---
+++ patterns/merge_intervals/conflicting_appointments.rb
@@ -0,0 +1,9 @@
+# Given an array of intervals representing ‘N’ appointments, find out if a person can attend all the appointments.
+
+def can_attend_all_appointments(intervals)
+
+end
+
+puts "Can attend all appointments: " + can_attend_all_appointments([[1, 4], [2, 5], [7, 9]]).to_s
+puts "Can attend all appointments: " + can_attend_all_appointments([[6, 7], [2, 4], [8, 12]]).to_s
+puts "Can attend all appointments: " + can_attend_all_appointments([[4, 5], [2, 3], [3, 6]]).to_s
