58fc72a83deb480c3744d2c5d914cc5c0379563d
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2020-12-02 09:51:02 -0500

    use && to enforce both conditions to be truthy

--- patterns/merge_intervals/find_intersection_intervals.rb
+++ patterns/merge_intervals/find_intersection_intervals.rb
@@ -8,15 +8,17 @@ def merge(intervals_a, intervals_b)
 	while i < intervals_a.length and j < intervals_b.length
 
 		# check if intervals overlap and intervals_a[i]'s start time lies within the other intervals_b[j]
-		a_overlaps_b = intervals_a[i][start_point] >= intervals_b[j][start_point] and intervals_a[i][start_point] <= intervals_b[j][end_point]
+		a_overlaps_b = intervals_a[i][start_point] >= intervals_b[j][start_point] && intervals_a[i][start_point] <= intervals_b[j][end_point]
 
 		# check if intervals overlap and intervals_b[j]'s start time lies within the other intervals_a[i]
-		b_overlaps_a = intervals_b[j][start_point] >= intervals_a[a][start_point] and intervals_b[j][start_point] <= intervals_a[i][end_point]
+		b_overlaps_a = intervals_b[j][start_point] >= intervals_a[i][start_point] && intervals_b[j][start_point] <= intervals_a[i][end_point]
 
 		# store the the intersection part
 
-		if a_overlaps_b or b_overlaps_a
-			merged.append([[intervals_a[i][start_point], intervals_b[j][start_point]].max, [intervals_a[i][end_point],intervals_b[j][end_point]].min])
+		if a_overlaps_b || b_overlaps_a
+			new_start = [intervals_a[i][start_point], intervals_b[j][start_point]].max
+			new_end = [intervals_a[i][end_point], intervals_b[j][end_point]].min
+			merged.append([new_start, new_end])
 		end
 
 		# move next from the interval which is finishing first
@@ -36,3 +38,6 @@ end
 
 puts "Intervals after inserting the new interval: #{merge([[1, 3], [5, 6], [7, 9]], [[2, 3], [5, 7]]).to_s}"
 puts "Intervals after inserting the new interval: #{merge([[1, 3], [5, 7], [9, 12]], [[5, 10]]).to_s}"
+
+# Intervals Intersection: [[2, 3], [5, 6], [7, 7]]
+# Intervals Intersection: [[5, 7], [9, 10]]
