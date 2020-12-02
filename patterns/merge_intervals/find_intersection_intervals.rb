8ad7eceb4aab65bb76f29a2b73f1ff569c13d3fc
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2020-12-02 09:24:47 -0500

    setup

--- patterns/merge_intervals/find_intersection_intervals.rb
+++ patterns/merge_intervals/find_intersection_intervals.rb
@@ -1,6 +1,21 @@
 # Given two lists of intervals, find the intersection of these two lists. Each list consists of disjoint intervals sorted on their start time.
 
 def merge(intervals_a, intervals_b)
+	merged = []
+
+	i, j, start_point, end_point = 0,0,0,1
+
+	while i < intervals_a.length and j < intervals_b.length
+
+		# check if intervals overlap and intervals_a[i]'s start time lies within the other intervals_b[j]
+
+		# check if intervals overlap and intervals_a[j]'s start time lies within the other intervals_b[i]
+
+		# store the the intersection part
+
+		# move next from the interval which is finishing first
+
+	end
 
 end
 
