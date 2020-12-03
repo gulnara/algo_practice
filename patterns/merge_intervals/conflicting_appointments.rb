a50fd31b0b9595c71ac0124173c3d0f1ae666eb1
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2020-12-03 08:29:43 -0500

    initial problem solution

--- patterns/merge_intervals/conflicting_appointments.rb
+++ patterns/merge_intervals/conflicting_appointments.rb
@@ -1,6 +1,15 @@
 # Given an array of intervals representing ‘N’ appointments, find out if a person can attend all the appointments.
 
 def can_attend_all_appointments(intervals)
+	intervals = intervals.sort_by {|el| el[0] }
+	start_point, end_point = 0, 1
+
+	for i in 1..(intervals.length - 1)
+		if intervals[i][start_point] < intervals[i - 1][end_point]
+			return false
+		end
+	end
+	return true
 
 end
 
