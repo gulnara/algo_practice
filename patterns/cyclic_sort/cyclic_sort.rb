88b66e8ab67dbf1b43f7464112cfd9a61870f62d
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2020-12-04 08:19:35 -0500

    added solution

--- patterns/cyclic_sort/cyclic_sort.rb
+++ patterns/cyclic_sort/cyclic_sort.rb
@@ -6,7 +6,17 @@
 # Output: [1, 2, 3, 4, 5, 6]
 
 def cyclic_sort(nums)
+	i = 0
 
+	while i < nums.length
+		j = nums[i] - 1
+		if nums[i] != nums[j]
+			nums[i], nums[j] = nums[j], nums[i]
+		else
+			i += 1
+		end
+	end
+	return nums
 end
 
 puts cyclic_sort([3, 1, 5, 4, 2])
