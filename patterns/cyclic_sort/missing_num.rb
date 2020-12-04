a92d6f01a891e2f721db71c599fdc82249535ac3
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2020-12-04 08:30:13 -0500

    find missing num problem setup

---
+++ patterns/cyclic_sort/missing_num.rb
@@ -0,0 +1,11 @@
+# We are given an array containing ‘n’ distinct numbers taken from the range 0 to ‘n’. Since the array has only ‘n’ numbers out of the total ‘n+1’ numbers, find the missing number.
+
+# Input: [4, 0, 3, 1]
+# Output: 2
+
+def find_missing_number(nums)
+
+end
+
+puts find_missing_number([4, 0, 3, 1])
+puts find_missing_number([8, 3, 5, 2, 4, 6, 0, 1])
