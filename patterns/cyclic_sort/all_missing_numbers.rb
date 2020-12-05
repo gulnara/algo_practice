a57a6eb4f6b4eeaefd45da525f51151f1c39028d
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2020-12-05 10:17:41 -0500

    find all missing

---
+++ patterns/cyclic_sort/all_missing_numbers.rb
@@ -0,0 +1,14 @@
+# We are given an unsorted array containing numbers taken from the range 1 to ‘n’. The array can have duplicates, which means some numbers will be missing. Find all those missing numbers.
+
+# Input: [2, 3, 1, 8, 2, 3, 5, 1]
+# Output: 4, 6, 7
+# Explanation: The array should have all numbers from 1 to 8, due to duplicates 4, 6, and 7 are missing.
+
+def find_missing_numbers(nums)
+
+
+end
+
+puts find_missing_numbers([2, 3, 1, 8, 2, 3, 5, 1])
+puts find_missing_numbers([2, 4, 1, 2])
+puts find_missing_numbers([2, 3, 2, 1])
