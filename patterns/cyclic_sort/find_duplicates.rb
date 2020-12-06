0430edc2f33cf8c4e6674db654e92aed3a5382e8
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2020-12-06 10:26:04 -0500

    find duplicates problem setup

---
+++ patterns/cyclic_sort/find_duplicates.rb
@@ -0,0 +1,8 @@
+# We are given an unsorted array containing ‘n+1’ numbers taken from the range 1 to ‘n’. The array has only one duplicate but it can be repeated multiple times. Find that duplicate number without using any extra space. You are, however, allowed to modify the input array.
+
+# Input: [1, 4, 4, 3, 2]
+# Output: 4
+
+def find_duplicate(nums)
+
+end
