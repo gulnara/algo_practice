e34ae1bad2b0db46f326cbdf8336c0e9b3daf8af
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2020-12-06 10:21:00 -0500

    initial solution

--- patterns/cyclic_sort/all_missing_numbers.rb
+++ patterns/cyclic_sort/all_missing_numbers.rb
@@ -6,9 +6,28 @@
 
 def find_missing_numbers(nums)
 
+	i = 0
 
+	while i < nums.length
+		j = nums[i] - 1
+		if nums[i] != nums[j]
+			nums[i], nums[j] = nums[j], nums[i]
+		else
+			i += 1
+		end
+	end
+
+	missing = []
+
+	for i in 0..(nums.length-1)
+		if nums[i] != i
+			missing.append(nums[i])
+		end
+	end
+
+	return missing
 end
 
-puts find_missing_numbers([2, 3, 1, 8, 2, 3, 5, 1])
-puts find_missing_numbers([2, 4, 1, 2])
+# puts find_missing_numbers([2, 3, 1, 8, 2, 3, 5, 1])
+# puts find_missing_numbers([2, 4, 1, 2])
 puts find_missing_numbers([2, 3, 2, 1])
