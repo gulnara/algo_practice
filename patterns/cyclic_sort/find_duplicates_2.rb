cbd195ca77e0eed9afb8e7f1a063b9e0c6cb39fa
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2020-12-27 09:03:10 -0500

    solution

--- patterns/cyclic_sort/find_duplicates_2.rb
+++ patterns/cyclic_sort/find_duplicates_2.rb
@@ -5,4 +5,27 @@
 
 def find_all_duplicates(nums)
 
+	i = 0 
+
+	while i < nums.length
+		j = nums[i] - 1
+		if nums[i] != nums[j]
+			nums[i], nums[j] = nums[j], nums[i]
+		else
+			i += 1
+		end
+
+	end
+
+	dups = []
+
+	for i in 0..(nums.length - 1) do 
+		if nums[i] != i + 1
+			dups.append(nums[i])
+		end
+	end
+
+	return dups 
 end
+
+puts find_all_duplicates([3, 4, 4, 5, 5])
