b5440804bbd8d5b3a57f62c71b112a6dc3ebbbd9
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2020-12-27 08:58:42 -0500

    all duplicate numbers

---
+++ patterns/cyclic_sort/find_duplicates_2.rb
@@ -0,0 +1,8 @@
+# We are given an unsorted array containing ‘n’ numbers taken from the range 1 to ‘n’. The array has some numbers appearing twice, find all these duplicate numbers without using any extra space.
+
+# Input: [3, 4, 4, 5, 5]
+# Output: [4, 5]
+
+def find_all_duplicates(nums)
+
+end
