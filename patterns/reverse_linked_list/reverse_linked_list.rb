1692c10605a0e6dc9fcbc2df01d6d259fc53e789
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2020-12-27 10:11:59 -0500

    problem setup;

--- patterns/reverse_linked_list/reverse_linked_list.rb
+++ patterns/reverse_linked_list/reverse_linked_list.rb
@@ -1,3 +1,27 @@
 # Given the head of a Singly LinkedList, reverse the LinkedList. Write a function to return the new head of the reversed LinkedList.
 
+class Node
 
+	attr_accessor :value, :next
+
+	def initialize(value)
+		@value = value
+		@next = nil
+	end
+
+end
+
+def reverse(head)
+
+
+
+end
+
+
+head = Node(2)
+head.next = Node(4)
+head.next.next = Node(6)
+head.next.next.next = Node(8)
+head.next.next.next.next = Node(10)
+result = reverse(head)
+puts result.value
