0f3819a74fd2783a9d76e7c23b03d03ed31ffcce
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2020-12-30 10:16:02 -0500

    initial solution

--- patterns/dfs/binary_tree_sum.rb
+++ patterns/dfs/binary_tree_sum.rb
@@ -3,7 +3,7 @@
 
 class TreeNode
 
-	attr_accessor :node, :left, :right
+	attr_accessor :value, :left, :right
 
 	def initialize(value)
 		@value = value
@@ -14,7 +14,16 @@ end
 
 
 def has_path(root, sum)
+	if root.nil?
+		return false
+	end
 
+	if root.value == sum and root.left.nil? and root.right.nil?
+		return true
+	end
+
+	has_path(root.right, sum - root.value)
+	has_path(root.left, sum - root.value)
 
 end
 
@@ -25,5 +34,5 @@ root.right = TreeNode.new(1)
 root.left.left = TreeNode.new(9)
 root.right.left = TreeNode.new(10)
 root.right.right = TreeNode.new(5)
-print("Tree has path: " + str(has_path(root, 23)))
-print("Tree has path: " + str(has_path(root, 16)))
+puts "Tree has path: " + has_path(root, 23).to_s
+puts "Tree has path: " + has_path(root, 16).to_s
