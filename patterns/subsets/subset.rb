630d6b6d4be82cc4a1146e157edac266c446de15
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2021-01-03 16:42:32 -0500

    solution

--- patterns/subsets/subset.rb
+++ patterns/subsets/subset.rb
@@ -6,11 +6,20 @@
 
 
 def find_subsets(nums)
+	subsets =[]
+	subsets.append([])
 
+	for current_num in nums
+		n = subsets.length
+		for i in 0..(n - 1)
+			set1 = Array.new(subsets[i])
+			set1.append(current_num)
+			subsets.append(set1)
+		end
+	end
+
+	return subsets
 end
-
-
-
 
 puts "Here is the list of subsets: " + find_subsets([1, 3]).to_s
 puts "Here is the list of subsets: " + find_subsets([1, 5, 3]).to_s
