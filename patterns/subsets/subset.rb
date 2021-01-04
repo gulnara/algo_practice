ed6a290e1cefeb4e38ea6d7e65e461ec67701347
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2021-01-04 08:58:04 -0500

    duplicate subsets

--- patterns/subsets/subset.rb
+++ patterns/subsets/subset.rb
@@ -11,12 +11,23 @@ def find_subsets(nums)
 	subsets =[]
 	subsets.append([])
 
-	for current_num in nums
+	for current_num in nums 
+		1
+		3
 		n = subsets.length
+		1
+		2
 		for i in 0..(n - 1)
 			set1 = Array.new(subsets[i])
+			[]
+			[]
+			[1]
 			set1.append(current_num)
+			[1]
+			[3]
+			[1,3]
 			subsets.append(set1)
+			[[],[1],[3], [1,3]]
 		end
 	end
 
@@ -24,4 +35,4 @@ def find_subsets(nums)
 end
 
 puts "Here is the list of subsets: " + find_subsets([1, 3]).to_s
-puts "Here is the list of subsets: " + find_subsets([1, 5, 3]).to_s
+# puts "Here is the list of subsets: " + find_subsets([1, 5, 3]).to_s
---
+++ patterns/subsets/subset_duplicates.rb
@@ -0,0 +1,13 @@
+# Given a set of numbers that might contain duplicates, find all of its distinct subsets.
+
+# Input: [1, 3, 3]
+# Output: [], [1], [3], [1,3], [3,3], [1,3,3]
+
+
+def find_subsets(nums)
+
+end
+
+
+puts "Here is the list of subsets: " + find_subsets([1, 3, 3]).to_s
+puts "Here is the list of subsets: " + find_subsets([1, 5, 3, 3]).to_s
