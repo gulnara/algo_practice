4a1c5d6a59c3c43ae5ca0f59702f69fc7e38bc96
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2021-01-05 08:39:14 -0500

    order agnostic binary search

---
+++ patterns/binary_search/order_agnostic_bs.rb
@@ -0,0 +1,20 @@
+# Given a sorted array of numbers, find if a given number ‘key’ is present in the array. Though we know that the array is sorted, we don’t know if it’s sorted in ascending or descending order. You should assume that the array can have duplicates.
+
+# Write a function to return the index of the ‘key’ if it is present in the array, otherwise return -1.
+
+
+# Input: [4, 6, 10], key = 10
+# Output: 2
+
+# Input: [10, 6, 4], key = 10
+# Output: 0
+
+def binary_search(arr, key)
+
+end
+
+
+puts binary_search([4, 6, 10], 10)
+puts binary_search([1, 2, 3, 4, 5, 6, 7], 5)
+puts binary_search([10, 6, 4], 10)
+puts binary_search([10, 6, 4], 4)
--- patterns/subsets/subset_duplicates.rb
+++ patterns/subsets/subset_duplicates.rb
@@ -14,16 +14,24 @@ def find_subsets(nums)
 
 	for i in 0..(nums.length - 1)
 		start_index = 0
-
+		0
+		0
 		if i > 0 and nums[i] == nums[i - 1]
 			start_index = end_index + 1
 		end
 
 		end_index = subsets.length - 1
+		0
+		1
 		for j in start_index..end_index
 			subset1 = Array.new(subsets[j])
+			[]
+			[]
 			subset1.append(nums[i])
+			[1]
+		[3]
 			subsets.append(subset1)
+			[[],[1],[3]]
 		end
 	end
 
