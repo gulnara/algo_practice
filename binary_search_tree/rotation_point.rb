e969c383afecd93503b9463cbac19474423b530c
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2021-01-09 15:32:46 -0500

    rotation point

---
+++ binary_search_tree/rotation_point.rb
@@ -0,0 +1,21 @@
+# Write a method for finding the index of the "rotation point," which is where I started working from the beginning of the dictionary. This array is huge (there are lots of words I don't know) so we want to be efficient here.
+
+words = [
+  'ptolemaic',
+  'retrograde',
+  'supplant',
+  'undulate',
+  'xenoepist',
+  'asymptote',  # <-- rotates here!
+  'babka',
+  'banoffee',
+  'engender',
+  'karpatka',
+  'othellolagkage',
+]
+
+def find_rotation_point(words)
+
+
+end
+
