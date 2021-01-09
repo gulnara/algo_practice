a00e6f8f8b367c8adc2797700a77c69d8479c632
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2021-01-09 15:35:45 -0500

    initial solution

--- binary_search_tree/rotation_point.rb
+++ binary_search_tree/rotation_point.rb
@@ -16,6 +16,23 @@ words = [
 
 def find_rotation_point(words)
 
+	first_word = words.first
+
+	floor_index = 0
+	ceiling_index = words.length - 1
+
+	while floor_index < ceiling_index
+
+		random_guess = (floor_index + ceiling_index) / 2
+
+		if words[random_guess] >= first_word
+			floor_index = random_guess
+		else
+			ceiling_index = random_guess
+		end
+	end
+
+	return ceiling_index if floor_index + 1 == ceiling_index
 
 end
 
