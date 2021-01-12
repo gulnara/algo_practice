70790d48b28e37e587d829bbc4825ef56de50001
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2021-01-12 08:00:41 -0500

    added print list

--- patterns/reverse_linked_list/reverse_alt_linked_list.py
+++ patterns/reverse_linked_list/reverse_alt_linked_list.py
@@ -9,6 +9,30 @@ class Node:
         self.value = value
         self.next = next
 
+    def print_list(self):
+        temp = self
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+        print()
+
 def reverse_alternate_k_elements(head, k):
 
     return head
+
+
+def main():
+    head = Node(1)
+    head.next = Node(2)
+    head.next.next = Node(3)
+    head.next.next.next = Node(4)
+    head.next.next.next.next = Node(5)
+    head.next.next.next.next.next = Node(6)
+    head.next.next.next.next.next.next = Node(7)
+    head.next.next.next.next.next.next.next = Node(8)
+    head.print_list()
+    result = reverse_alternate_k_elements(head, 2)
+    result.print_list()
+
+
+main()
