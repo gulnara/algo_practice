0965b08480e8f789f8fd690ca4356c2c229b4b1a
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2021-01-12 07:50:36 -0500

    added python version of reverse a linked list

---
+++ patterns/reverse_linked_list/reverse_alt_linked_list.py
@@ -0,0 +1,14 @@
+# Given the head of a LinkedList and a number ‘k’, reverse every alternating ‘k’ sized sub-list starting from the head.
+
+# If, in the end, you are left with a sub-list with less than ‘k’ elements, reverse it too.
+
+
+class Node:
+
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+def reverse_alternate_k_elements(head, k):
+
+    return head
